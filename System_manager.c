33479653a9745c22ce5804399ebfea62a7a1b479
GuiFFaria <guifaria02@gmail.com>
2022-04-17 00:25:03 +0100

    first commit

---
+++ System_manager.c
@@ -0,0 +1,123 @@
+#include "header.h"
+
+
+
+void escreverLog(char mensagem[]) {
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+
+    FILE *f = fopen("log.txt","a");
+    printf("%d:%d:%d %s\n", tm->tm_hour, tm->tm_min, tm->tm_sec, mensagem);
+    fprintf(f, "%d:%d:%d %s\n", tm->tm_hour, tm->tm_min, tm->tm_sec, mensagem);
+    fclose(f);
+}
+
+int lerFicheiro() { 
+
+    FILE *file;
+
+    char line[SIZE];
+    int i = 0;
+    int config[10];
+    char *token;
+    char s[3] = ", ";
+
+    // Abertura do ficheiro "input.txt" para leitura
+    file = fopen("input.txt", "r");
+
+    // ficheiro não encontrado
+    if (file == NULL) {
+        sem_wait(mutex_log);
+        escreverLog("FILE 'input.txt' NOT FOUND");
+        sem_post(mutex_log);
+        return 1;
+    }
+
+    while (fgets(line, SIZE, file) != NULL) {
+        token = strtok(line,s);
+
+        while( token != NULL ) {
+            config[i] = strtol(token, &token, 10);
+            token = strtok(NULL, s);
+            i++;
+        }
+    }
+
+    //Verificação do mínimo de 2 Edge Servers
+    if (config[3] < 2) {
+        sem_wait(mutex_log);
+        escreverLog("NUMBER OF EDGE SERVERS NOT ENOUGH");
+        sem_post(mutex_log);
+        return 1;
+    }
+    sem_wait(mutex_write);
+    shared_data -> num_slots = config[0];
+    shared_data -> max_wtime = config[1];
+    shared_data -> num_servers = config[2];
+    int i;
+    int j = 3;
+    for(i = 0; i < config[2]; i++){
+        shared_data -> servers[i].name = config[j];
+        shared_data -> servers[i].vcpu1 = config[j+1];
+        shared_data -> servers[i].vcpu2 = config[j+2];
+        j++;
+    }
+    sem_post(mutex_write);
+
+    fclose(file);
+    return 0;
+}
+
+void iniciar() {
+
+    //remove ficheiro log, caso exista
+    remove("log.txt");
+
+    //create shared memory
+    if ((shmid = shmget(IPC_PRIVATE, sizeof(Data), IPC_CREAT | 0700)) < 0) {
+        perror("Error in shmget with IPC_CREAT\n");
+        exit(1);
+    }
+
+    // Attach shared memory
+    if ((shared_data = (Data *) shmat(shmid, NULL, 0)) == (Data *)-1) {
+        perror("Shmat error.\n");
+        exit(1);
+    }
+
+    sem_unlink("MUTEX_LOG");//remove o nome do semáforo
+    mutex_log = sem_open("MUTEX_LOG",O_CREAT|O_EXCL,0700,1);//retorna o endereço do novo semáforo
+
+    sem_unlink("MUTEX_WRITE");//remove o nome do semáforo
+    mutex_write = sem_open("MUTEX_WRITE",O_CREAT|O_EXCL,0700,1);//retorna o endereço do novo semáforo
+
+    
+    
+    sem_wait(mutex_log);//locks the semaphore
+    escreverLog("OFFLOAD SIMULATOR STARTING");
+    sem_post(mutex_log);//unlocks the semaphore
+}
+//Processo que gera tarefas com um determinado nº de milhares de instruções a executar num intervalo de tempo
+int MobileNode(){
+
+}
+//Cria o named pipe onde os processos criados anteriormente irão escrever
+int CreateNamedPipe(){
+    int fd;
+    char * namedPipe = "C:/Users/guifa/Universidade/SO/Projeto_SO/TASK_PIPE";
+
+    mkfifo(namedPipe, 0666);
+
+    char arr1[80], arr2[80];
+
+    while(1){
+
+        fd = open(namedPipe, O_WRONLY);
+
+        fgets(arr2, 80, stdin);
+
+        write(fd, arr2, strlen(arr2) + 1);
+        close(fd);
+    }
+
+}
---
+++ header.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/fcntl.h>
+
+#define SIZE 100
+#define N 10
+#define MAX_LENGTH 256
+#define NUM_SERVERS 100
+
+typedef struct memory {
+    int num_slots; //Número de slots na fila interna do Task Manager
+    int max_wtime; //Tempo máximo para que o processo Monitor eleve o nível de performance dos Edge Servers
+    int num_servers; //Número de Edge Servers
+    EdgeServer servers[NUM_SERVERS]; //array of servers
+
+    pthread_mutex_t mutex;
+
+} Data;
+
+//struct with server's information
+typedef struct server {
+    char name[256];
+    int vcpu1;
+    int vcpu2;
+
+} EdgeServer;
+
+
+Data *shared_data;
+sem_t * mutex_log;
+sem_t * mutex_write;
+int shmid;
